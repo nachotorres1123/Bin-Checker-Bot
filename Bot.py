2783ab3fbc68d1ede6e5d63ba16a84fb74d46e77
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-12 03:46:00 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -60,7 +60,7 @@ async def bin(_, m: Message):
             inputm = m.text.split(None, 1)[1]
             bincode = 6
             ask = inputm[:bincode]
-            req = requests.get(f"https://madbin.herokuapp.com/api/{ask}").json()
+            req = requests.get(f"api.apilayer.com/G6wqRUaOVzlvwlvavzHeefh2j1exTjse/{ask}").json()
             res = req["result"]
 
             if res == False:
