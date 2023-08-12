30d183412946f18ee452cd4cc37e20e3a6ade206
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-12 04:13:08 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -43,14 +43,14 @@ async def start(_, m: Message):
 @Bot.on_message(filters.command("help"))
 async def help(_, m: Message):
     await m.reply_text(
-        "/start - **To check bot alive**.\n/help - **To see help menu.**\n/bin [qoury] - **To check Bin is valide or Invalid.**"
+        "/start - To check bot alive.\n/help - To see help menu.\n/bin [qoury] - To check Bin is valide or Invalid."
     )
 
 
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
     if len(m.command) < 2:
-        msg = await m.reply_text("Please Provide a Bin!\nEx:- `/bin 401658`")
+        msg = await m.reply_text("Please Provide a Bin!\nEx:- /bin 401658")
         await sleep(15)
         await msg.delete()
 
@@ -60,39 +60,8 @@ async def bin(_, m: Message):
             inputm = m.text.split(None, 1)[1]
             bincode = 6
             ask = inputm[:bincode]
-            req = requests.get(f"api.apilayer.com/G6wqRUaOVzlvwlvavzHeefh2j1exTjse/{ask}").json()
+            req = requests.get(f"https://madbin.herokuapp.com/api/{ask}").json()
             res = req["result"]
-$curl = curl_init();
-
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://bin-ip-checker.p.rapidapi.com/?bin=448590",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "POST",
-	CURLOPT_POSTFIELDS => json_encode([
-		'bin' => '448590'
-	]),
-	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: bin-ip-checker.p.rapidapi.com",
-		"X-RapidAPI-Key: 8591782eb8msh35855b7b3e23774p11ee22jsncda4429bc4ec",
-		"content-type: application/json"
-	],
-]);
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
-}
-            
 
             if res == False:
                 return await mafia.edit("❌ #INVALID_BIN ❌\n\nPlease provide a valid bin.")
@@ -111,12 +80,12 @@ if ($err) {
 
             mfrom = m.from_user.mention
             caption = f"""
-    ╔ Valid :- `{res} ✅`\n╚ Bin :- `{bi}`\n\n╔ Brand :- `{ve}`\n╠ Type :- `{ty}`\n╚ Level :- `{le}`\n\n╔ Bank :- `{ban} ({co})`\n╠ Country :- `{nm} {em}`\n╠ Alpha2 :- `{cod}`\n╚ DialCode :- `{dial}`\n\n**↠ Checked By :-** {mfrom}\n**↠ __Bot By :-** [Denuwan](https://github.com/ImDenuwan/Bin-Checker-Bot)__
+    ╔ Valid :- {res} ✅\n╚ Bin :- {bi}\n\n╔ Brand :- {ve}\n╠ Type :- {ty}\n╚ Level :- {le}\n\n╔ Bank :- {ban} ({co})\n╠ Country :- {nm} {em}\n╠ Alpha2 :- {cod}\n╚ DialCode :- {dial}\n\n↠ Checked By :- {mfrom}\n↠ Bot By :- [Denuwan](https://github.com/ImDenuwan/Bin-Checker-Bot)
     """
             await mafia.edit(caption, disable_web_page_preview=True)
             
         except Exception as e:
-            await bot.reply_text(f"**Oops Error!**\n{e}\n\n**Report This Bug to Bot Owner.**")
+            await bot.reply_text(f"Oops Error!\n{e}\n\nReport This Bug to Bot Owner.")
 
 print("Bot IS Alive Now")
 
