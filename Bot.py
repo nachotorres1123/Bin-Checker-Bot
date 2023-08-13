1e0c49fc56ec6aebad61960f0f6bb487641c7b1e
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-13 05:39:06 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,14 +9,12 @@ from pyrogram.types import (
     InlineKeyboardMarkup
 )
 
-
 Bot = Client(
     ":memory:",
     api_hash=config.API_HASH,
     api_id=config.API_ID,
     bot_token=config.BOT_TOKEN,
 )
-
 
 @Bot.on_message(filters.command("start"))
 async def start(_, m: Message):
@@ -26,61 +24,36 @@ async def start(_, m: Message):
             [
                 InlineKeyboardButton("Channel", url="https://t.me/NtEasyM0ney"),
                 InlineKeyboardButton("Support", url="https://t.me/NtEasyMoney"),
-                InlineKeyboardButton("Channel", url="https://t.me/NtEasyM0ney"),
-                InlineKeyboardButton("Support", url="https://t.me/NtEasyMoney"),
             ],
             [
                 InlineKeyboardButton(
-                    "Source code", url="https://t.me/NtEasyM0ney"
+                    "Source code", url="https://github.com/ImDenuwan/Bin-Checker-Bot"
                 )
             ],
-            [
-                InlineKeyboardButton(
-                    "Source code", url="https://t.me/NtEasyM0ney"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "Source code", url="https://t.me/NtEasyM0ney"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "Source code", url="https://t.me/NtEasyM0ney"
-                )
-            ],
-
-
-
-            
         ]
     )
     await m.reply_text(
-        f"Hi! {messy} \nI can Check bins Valid or Invalid.\n\nTo see more check /help command",
+        f"Hi! {messy}\nI can check if a Bin is Valid or Invalid.\n\nTo see more, use the /help command.",
         reply_markup=keyboard,
     )
-
 
 @Bot.on_message(filters.command("help"))
 async def help(_, m: Message):
     await m.reply_text(
-        "/start - To check bot alive.\n/help - To see help menu.\n/bin [qoury] - To check Bin is valide or Invalid."
+        "/start - Check if the bot is alive.\n/help - See the help menu.\n/bin [query] - Check if a Bin is valid or invalid."
     )
-
 
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
     if len(m.command) < 2:
-        msg = await m.reply_text("Please Provide a Bin!\nEx:- /bin 401658")
+        msg = await m.reply_text("Please provide a Bin!\nExample: /bin 401658")
         await sleep(15)
         await msg.delete()
-
     else:
         try:
-            mafia = await m.reply_text("processing...")
+            mafia = await m.reply_text("Processing...")
             inputm = m.text.split(None, 1)[1]
-            bincode = 6
-            ask = inputm[:bincode]
+            bincode = len(inputm)
             
             api_key = "8591782eb8msh35855b7b3e23774p11ee22jsncda4429bc4ec"
             url = "https://lookup.bincheck.io/api/bin"
@@ -88,22 +61,22 @@ async def bin(_, m: Message):
                 "Accept-Version": "3",
                 "Authorization": f"Bearer {api_key}",
             }
-            payload = {"bin": ask}
+            payload = {"bin": inputm}
             response = requests.post(url, headers=headers, json=payload)
             
             if response.status_code == 200:
                 data = response.json()
                 bank_name = data["bank"]["name"]
                 card_brand = data["scheme"]
-                mfrom = m.from_user .mention
+                mfrom = m.from_user.mention
                 caption = f"""
-    ╔ Bank Name :- {bank_name}\n╚ Card Brand :- {card_brand}\n\n↠ Checked By :- {m.from_user.mention}\n↠ Bot By :- {mfrom}\n↠ Bot By :- [Denuwan](https://github.com/ImDenuwan/Bin-Checker-Bot)
-    """
-            await mafia.edit(caption, disable_web_page_preview=True)
+Bank Name: {bank_name}\nCard Brand: {card_brand}\n\nChecked By: {m.from_user.mention}\nBot By: {mfrom}\nBot Source Code: [GitHub](https://github.com/ImDenuwan/Bin-Checker-Bot)
+"""
+                await m.edit_message_text(caption, disable_web_page_preview=True)
             
         except Exception as e:
-            await bot.reply_text(f"Oops Error!\n{e}\n\nReport This Bug to Bot Owner.")
+            await m.reply_text(f"Oops! An error occurred:\n{e}\n\nPlease report this bug to the bot owner.")
 
-print("Bot IS Alive Now")
+print("Bot is alive now!")
 
 Bot.run()
