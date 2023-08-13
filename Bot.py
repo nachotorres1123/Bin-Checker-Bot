bf0b3b432a8ac9c470b6f41217ec04ccceffe8ef
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-13 02:46:32 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,17 +4,19 @@ from configs import config
 from asyncio import sleep
 
 from pyrogram.types import (
-    Message,
-    InlineKeyboardButton,
+    Message, 
+    InlineKeyboardButton, 
     InlineKeyboardMarkup
 )
 
+
 Bot = Client(
     ":memory:",
-    api_hash=config.api_hash,
-    api_id=config.api_id,
-    bot_token=config.bot_token,
+    api_hash=config.API_HASH,
+    api_id=config.API_ID,
+    bot_token=config.BOT_TOKEN,
 )
+
 
 @Bot.on_message(filters.command("start"))
 async def start(_, m: Message):
@@ -35,11 +37,6 @@ async def start(_, m: Message):
                     "prueba 2", url="https://t.me/NtEasyM0ney"
                 )
             ],
-            [
-                InlineKeyboardButton(
-                    "Prueba 3", url="https://t.me/NtEasyM0ney"
-                )
-            ],
         ]
     )
     await m.reply_text(
@@ -47,11 +44,13 @@ async def start(_, m: Message):
         reply_markup=keyboard,
     )
 
+
 @Bot.on_message(filters.command("help"))
 async def help(_, m: Message):
     await m.reply_text(
         "/start - To check bot alive.\n/help - To see help menu.\n/bin [qoury] - To check Bin is valide or Invalid."
     )
+
 
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
@@ -66,32 +65,33 @@ async def bin(_, m: Message):
             inputm = m.text.split(None, 1)[1]
             bincode = 6
             ask = inputm[:bincode]
-            req = requests.get(f"https://api.bincodes.com/bin/json/0d6542332875d2012b12f3c9d17b8007/{ask}/").json()
-            da = req
-            print(da)
-            if "False" in str(da):
+            req = requests.get(f"https://madbin.herokuapp.com/api/{ask}").json()
+            res = req["result"]
+
+            if res == False:
                 return await mafia.edit("❌ #INVALID_BIN ❌\n\nPlease provide a valid bin.")
-            #da = req["data"]
-            bi = str(da["bin"])
-            ve = str(da["card"])
-            #ve = da["vendor"]
-            ty = str(da["type"])
-            le = str(da["level"])
-            ban = str(da["bank"])
-            co = str(da["country"])
-            cc = str(da["countrycode"])
-            #nm = cc["name"]
-            #em = cc["emoji"]
-            #cod = cc["code"]
-            #dial = cc["dialCode"]
+            da = req["data"]
+            bi = da["bin"]
+            ve = da["vendor"]
+            ty = da["type"]
+            le = da["level"]
+            ban = da["bank"]
+            co = da["country"]
+            cc = da["countryInfo"]
+            nm = cc["name"]
+            em = cc["emoji"]
+            cod = cc["code"]
+            dial = cc["dialCode"]
 
             mfrom = m.from_user.mention
-
             caption = f"""
-            ╔ Valid :- Yes ✅\n╚ Bin :- {bi}\n\n╔ Brand :- {ve}\n╠ Type :- {ty}\n╚ Level :- {le}\n\n╔ Bank :- {ban} ({co})\n╠ Country :- {co} {cc}\n╚ DialCode :- {da['phone']}\n\n↠ Checked By :- {str(mfrom)}\n↠ Bot By :- [Denuwan](https://t.me/Denuwanthi)"
-            """
-            await mafia.edit(caption, parse_mode="markdown")
-
+    ╔ Valid :- {res} ✅\n╚ Bin :- {bi}\n\n╔ Brand :- {ve}\n╠ Type :- {ty}\n╚ Level :- {le}\n\n╔ Bank :- {ban} ({co})\n╠ Country :- {nm} {em}\n╠ Alpha2 :- {cod}\n╚ DialCode :- {dial}\n\n↠ Checked By :- {mfrom}\n↠ Bot By :- [Denuwan](https://github.com/ImDenuwan/Bin-Checker-Bot)
+    """
+            await mafia.edit(caption, disable_web_page_preview=True)
+            
         except Exception as e:
-            print(str(e))
-            await mafia.edit("❌ #ERROR_OCCURED ❌\n\nSomething went wrong.")
+            await bot.reply_text(f"Oops Error!\n{e}\n\nReport This Bug to Bot Owner.")
+
+print("Bot IS Alive Now")
+
+Bot.run()
