1bca87c60e3dc7ee118b994f2096ddbe96ee0de3
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-13 07:09:39 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -17,89 +17,92 @@ Bot = Client(
 )
 
 @Bot.on_message(filters.command("start"))
-async def start(_, m: Message):
-    messy = m.from_user.mention
-    keyboard = InlineKeyboardMarkup(
+async def inicio(_, m: Message):
+    mencion_usuario = m.from_user.mention
+    teclado = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Channel", url="https://t.me/NtEasyM0ney"),
-                InlineKeyboardButton("Support", url="https://t.me/NtEasyMoney"),
+                InlineKeyboardButton("Canal", url="https://t.me/NtEasyM0ney"),
+                InlineKeyboardButton("Soporte", url="https://t.me/NtEasyMoney"),
             ],
             [
                 InlineKeyboardButton(
-                    "Source code", url="https://github.com/ImDenuwan/Bin-Checker-Bot"
+                    "Código fuente", url="https://github.com/ImDenuwan/Bin-Checker-Bot"
                 )
             ],
         ]
     )
     await m.reply_text(
-        f"Hi! {messy}\nI can check if a Bin is Valid or Invalid.\n\nTo see more, use the /help command.",
-        reply_markup=keyboard,
+        f"Hola, {mencion_usuario}\nPuedo verificar si un Bin es válido o inválido.\n\nPara ver más, usa el comando /ayuda.",
+        reply_markup=teclado,
     )
 
-@Bot.on_message(filters.command("help"))
-async def help(_, m: Message):
+@Bot.on_message(filters.command("ayuda"))
+async def ayuda(_, m: Message):
     await m.reply_text(
-        "/start - Check if the bot is alive.\n/help - See the help menu.\n/bin [query] - Check if a Bin is valid or invalid.\n/fetchdata - Fetch data from a URL."
+        "/inicio - Verificar si el bot está activo.\n/ayuda - Ver el menú de ayuda.\n/bin [consulta] - Verificar si un Bin es válido o inválido.\n/datos - Obtener datos de una URL."
     )
 
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
     if len(m.command) < 2:
-        msg = await m.reply_text("Please provide a Bin!\nExample: /bin 401658")
+        msg = await m.reply_text("¡Por favor, proporciona un Bin!\nEjemplo: /bin 401658")
         await sleep(15)
         await msg.delete()
     else:
         try:
-            mafia = await m.reply_text("Processing...")
-            inputm = m.text.split(None, 1)[1]
-            bin_code = inputm
+            mafia = await m.reply_text("Procesando...")
+            entrada = m.text.split(None, 1)[1]
+            codigo_bin = entrada
 
-            url = f"https://api.apilayer.com/bincheck/{bin_code}"
+            url = f"https://api.apilayer.com/bincheck/{codigo_bin}"
 
-            headers = {
+            cabeceras = {
                 "apikey": "G6wqRUaOVzlvwlvavzHeefh2j1exTjse"
             }
 
-            response = requests.get(url, headers=headers)
+            respuesta = requests.get(url, headers=cabeceras)
             
-            if response.status_code == 200:
-                data = response.json()
-                bank_name = data["bank"]["name"]
-                card_brand = data["scheme"]
-                mfrom = m.from_user.mention
-                caption = f"""
-Bank Name: {bank_name}\nCard Brand: {card_brand}\n\nChecked By: {m.from_user.mention}\nBot By: {mfrom}\nBot Source Code: [GitHub](https://github.com/ImDenuwan/Bin-Checker-Bot)
+            if respuesta.status_code == 200:
+                datos = respuesta.json()
+                try:
+                    nombre_banco = datos["bank"]["name"]
+                    marca_tarjeta = datos["scheme"]
+                    mencion_de = m.from_user.mention
+                    caption = f"""
+Nombre del banco: {nombre_banco}\nMarca de la tarjeta: {marca_tarjeta}\n\nVerificado por: {mencion_de}\nBot creado por: {mencion_de}\nCódigo fuente del bot: [GitHub](https://github.com/ImDenuwan/Bin-Checker-Bot)
 """
-                await mafia.edit_message_text(caption, disable_web_page_preview=True)
+                    await mafia.edit_message_text(caption, disable_web_page_preview=True)
+                except KeyError as e:
+                    await mafia.edit_message_text(f"Error: {e}\n\nRespuesta: {respuesta.text}")
             else:
-                await mafia.edit_message_text("Invalid Bin or an error occurred.")
+                await mafia.edit_message_text("Bin inválido o se produjo un error.")
             
         except Exception as e:
-            await m.reply_text(f"Oops! An error occurred:\n{e}\n\nPlease report this bug to the bot owner.")
+            await m.reply_text(f"¡Ups! Se produjo un error:\n{e}\n\nPor favor, informa este error al propietario del bot.")
 
-@Bot.on_message(filters.command("fetchdata"))  # Nuevo comando para solicitar datos
-async def fetch_data(_, m: Message):
+@Bot.on_message(filters.command("datos"))  # Nuevo comando para obtener datos
+async def obtener_datos(_, m: Message):
     try:
-        url = "https://api.apilayer.com/bincheck/{bin_code}"
-        payload = {}
-        headers = {
+        url = "https://api.apilayer.com/bincheck/{codigo_bin}"
+        datos_payload = {}
+        cabeceras = {
             "apikey": "G6wqRUaOVzlvwlvavzHeefh2j1exTjse"
         }
         
-        response = requests.get(url, headers=headers, data=payload)
+        respuesta = requests.get(url, headers=cabeceras, data=datos_payload)
         
-        status_code = response.status_code
-        result = response.text
+        codigo_estado = respuesta.status_code
+        resultado = respuesta.text
         
-        if status_code == 200:
-            await m.reply_text(f"Status Code: {status_code}\n\nResult:\n{result}")
+        if codigo_estado == 200:
+            await m.reply_text(f"Código de estado: {codigo_estado}\n\nResultado:\n{resultado}")
         else:
-            await m.reply_text(f"Failed to fetch data. Status Code: {status_code}")
+            await m.reply_text(f"No se pudo obtener los datos. Código de estado: {codigo_estado}")
             
     except Exception as e:
-        await m.reply_text(f"An error occurred:\n{e}")
+        await m.reply_text(f"Se produjo un error:\n{e}")
 
-print("Bot is alive now!")
+print("¡El bot está en línea!")
 
 Bot.run()
