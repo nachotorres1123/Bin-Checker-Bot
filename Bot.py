1dfb702bf4032d0578225e6bb31072ed46441cd7
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-14 00:47:32 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -40,7 +40,7 @@ def validate_credit_card(card_number):
         return "Inválida ❌"
 
 def generate_password(length=12):
-    characters = string.ascii_letters + string.digits + string.punctuation
+    characters = string.digits
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
