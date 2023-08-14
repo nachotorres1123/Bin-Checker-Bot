2626f14f1a783e6450df95086312dd2f4f8fbadf
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-14 02:33:52 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,5 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "21590558"))
     API_HASH = getenv("API_HASH", "9767814b790f12ad9a333a20bcaf1200")
     BOT_TOKEN = getenv("BOT_TOKEN", "6466372597:AAGTPhW8XVaW64O_BDeY7k7Tq6yZXuacYZg")
+    ACCESS_KEY = "@NtEasyMoney$12041123"
+
 
 config = Config()
