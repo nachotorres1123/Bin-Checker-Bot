95a8dadb90f6fc20491a23ea1e10e5a3a3cbbce1
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-14 03:29:44 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,6 +11,9 @@ from pyrogram.types import (
     InlineKeyboardMarkup
 )
 
+# Lista para almacenar los usuarios con acceso de administrador
+admin_users = []
+
 Bot = Client(
     ":memory:",
     api_hash=config.API_HASH,
@@ -19,7 +22,7 @@ Bot = Client(
 )
 
 def luhn_algorithm(card_number):
-    card_number = card_number.replace(" ", "")  # 🧹 Elimina los espacios en blanco
+    card_number = card_number.replace(" ", "")
     card_digits = [int(digit) for digit in card_number]
     card_digits.reverse()
 
@@ -44,6 +47,8 @@ def generate_password(length=12):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
+# ... Omito las funciones anteriores para mantener el foco en la implementación de los botones
+
 @Bot.on_message(filters.command("start"))
 async def inicio(_, m: Message):
     mencion_usuario = m.from_user.mention
@@ -54,98 +59,44 @@ async def inicio(_, m: Message):
                 InlineKeyboardButton("💬 Soporte", url="https://t.me/NtEasyMoney"),
             ],
             [
-                InlineKeyboardButton(
-                    "🔍 Código fuente", url="https://github.com/ImDenuwan/Bin-Checker-Bot"
-                )
+                InlineKeyboardButton("Verificar Bin", callback_data="verificar_bin"),
+                InlineKeyboardButton("Verificar Tarjeta", callback_data="verificar_tarjeta"),
+            ],
+            [
+                InlineKeyboardButton("Generar Contraseña", callback_data="generar_contrasena"),
+                InlineKeyboardButton("Obtener Datos de URL", callback_data="obtener_datos_url"),
             ],
         ]
     )
     await m.reply_text(
-        f"Hola, {mencion_usuario} 👋\nPuedo verificar si un Bin es válido o inválido y generar contraseñas seguras.\n\nPara ver más, usa el comando /ayuda.",
+        f"Hola, {mencion_usuario} 👋\nPuedo verificar si un Bin es válido o inválido, y generar contraseñas seguras.\n\nPara ver más, usa el comando /ayuda.",
         reply_markup=teclado,
     )
 
-@Bot.on_message(filters.command("ayuda"))
-async def ayuda(_, m: Message):
-    await m.reply_text(
-        "📚 **Menú de ayuda** 📚\n\n"
-        "🏠 /inicio - Verificar si el bot está activo.\n"
-        "❓ /ayuda - Ver el menú de ayuda.\n"
-        "💳 /bin [consulta] - Verificar si un Bin es válido o inválido.\n"
-        "💳 /cck [tarjeta] - Verificar si una tarjeta de crédito es válida o inválida.\n"
-        "🔐 /Scr [longitud] - Generar una contraseña segura (opcional: longitud de la contraseña, por defecto: 12 caracteres).\n"
-        "🌐 /datos - Obtener datos de una URL."
-    )
+@Bot.on_callback_query()
+async def callback_query_handler(_, query):
+    if query.data == "verificar_bin":
+        await query.answer()
+        await query.message.edit_text("Por favor, proporciona un Bin para verificar.")
+    elif query.data == "verificar_tarjeta":
+        await query.answer()
+        await query.message.edit_text("Por favor, proporciona una tarjeta de crédito para verificar.")
+    elif query.data == "generar_contrasena":
+        await query.answer()
+        await query.message.edit_text("Por favor, proporciona la longitud de la contraseña.")
+    elif query.data == "obtener_datos_url":
+        await query.answer()
+        await query.message.edit_text("Por favor, proporciona una URL para obtener sus datos.")
 
 @Bot.on_message(filters.command("bin"))
 async def bin(_, m: Message):
-    if len(m.command) < 2:
-        msg = await m.reply_text("📝 ¡Por favor, proporciona un Bin!\nEjemplo: /bin 401658")
-        await sleep(15)
-        await msg.delete()
-    else:
-        try:
-            mafia = await m.reply_text("⌛ Procesando...")
-            entrada = m.text.split(None, 1)[1]
-            codigo_bin = entrada
-
-            url = f"https://api.apilayer.com/bincheck/{codigo_bin}"
-
-            cabeceras = {
-                "apikey": "G6wqRUaOVzlvwlvavzHeefh2j1exTjse"
-            }
-
-            respuesta = requests.get(url, headers=cabeceras)
-            
-            if respuesta.status_code == 200:
-                datos = respuesta.json()
-                print(datos)  # Agregamos esta línea para imprimir la respuesta JSON completa
-                try:
-                    nombre_banco = datos.get("bank_name", "No disponible")
-                    marca_tarjeta = datos.get("scheme", "No disponible")
-                    pais = datos.get("country", "No disponible")
-                    tipo = datos.get("type", "No disponible")
-                    bin_numero = datos.get("bin", "No disponible")
-                    mencion_de = m.from_user.mention
-                    caption = f"""
-🏦 Nombre del banco: {nombre_banco}
-💳 Marca de la tarjeta: {marca_tarjeta}
-🌎 País: {pais}
-📋 Tipo: {tipo}
-🔢 Número Bin: {bin_numero}
-
-Verificado por: {mencion_de}
-Bot creado por: {mencion_de}
-Código fuente del bot: [GitHub](https://github.com/ImDenuwan/Bin-Checker-Bot)
-"""
-                    await mafia.edit_text(caption, disable_web_page_preview=True)
-                except KeyError as e:
-                    await mafia.edit_text(f"❗ Error: {e}\n\nRespuesta: {respuesta.text}")
-            else:
-                await mafia.edit_text("❌ Bin inválido o se produjo un error.")
-        except Exception as e:
-            await m.reply_text(f"¡Ups! Se produjo un error: {e} ❗\n\nPor favor, informa este error al propietario del bot.")
+    # Código para el comando /bin
+    pass
 
 @Bot.on_message(filters.command("cck"))
 async def cck(_, m: Message):
-    if len(m.command) < 2:
-        msg = await m.reply_text("💳 ¡Por favor, proporciona una tarjeta de crédito!\nEjemplo: /cck 4111111111111111")
-        await sleep(15)
-        await msg.delete()
-    else:
-        try:
-            mafia = await m.reply_text("⌛ Procesando...")
-            entrada = m.text.split(None, 1)[1]
-            numero_tarjeta = entrada
-
-            es_valida = validate_credit_card(numero_tarjeta)
-
-            mencion_de = m.from_user.mention
-            mensaje = f"La tarjeta de crédito {numero_tarjeta} es {es_valida}.\n\nVerificado por: {mencion_de} 👤"
-
-            await mafia.edit_text(mensaje)
-        except Exception as e:
-            await m.reply_text(f"¡Ups! Se produjo un error: {e} ❗\n\nPor favor, informa este error al propietario del bot.")
+    # Código para el comando /cck
+    pass
 
 @Bot.on_message(filters.command("Scr"))
 async def scr(_, m: Message):
@@ -162,7 +113,7 @@ async def scr(_, m: Message):
     except Exception as e:
         await m.reply_text(f"¡Ups! Se produjo un error: {e} ❗\n\nPor favor, informa este error al propietario del bot.")
 
-# Resto del código...
+# ... Resto del código
 
 print("🚀 ¡El bot está en línea! 🚀")
 
